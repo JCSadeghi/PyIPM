47f822cb5f8f4352b799e59c605800cd44d17b51
Jonathan Sadeghi <J.C.Sadeghi@liverpool.ac.uk>
2018-03-09 22:19:55 +0000

    Clean up basis matrix generation
    Change variable names, replace basis generation with sklearn code

--- PyIPM.py
+++ PyIPM.py
@@ -1,9 +1,9 @@
 import numpy as np
-
+from sklearn.preprocessing import PolynomialFeatures
 #==============================================================================
 #     This class is a port of the MATLAB code IntervalPredictorModel from
 #     OpenCossan
-#     
+#
 #     2018, Jonathan Sadeghi, COSSAN Working Group,
 #     University~of~Liverpool, United Kingdom
 #     See also:  http://cossan.co.uk/wiki/index.php/@IntervalPredictorModel
@@ -16,77 +16,60 @@ class PyIPM:
     def fit(self,trainingInput,trainingOutput):
         self.NFeatures=trainingInput.shape[1]
         self.NDataPoints=trainingInput.shape[0]
-        
+
         assert(trainingOutput.shape==(self.NDataPoints,)),'Number of input examples must equal number of output examples'
-         
-        from pyDOE import fullfact
-        self.exponentList=fullfact(np.int32((self.polynomialDegree+1)*np.ones((1,self.NFeatures))[0]))
-        self.Nexponents=self.exponentList.shape[0]       
-        
+
         self.InputScale=np.mean(np.abs(trainingInput),axis=0);
-        trainingInput=trainingInput/self.InputScale;   
-        
-        basis=np.zeros((self.NDataPoints,self.Nexponents))
-        
-        for j,exponentVector in enumerate(self.exponentList):
-            for i,inputSample in enumerate(trainingInput):
-                basis[i,j]=np.prod(inputSample**exponentVector)
-        
+        trainingInput=trainingInput/self.InputScale;
+
+        poly = PolynomialFeatures(self.polynomialDegree)
+        basis=poly.fit_transform(trainingInput)
+        self.Nterms=basis.shape[1]
+
         basisSum=np.mean(np.absolute(basis), axis=0)
-        objective=np.concatenate((-basisSum,basisSum))  
-        
-        constraintMat=np.zeros((2*self.NDataPoints+self.Nexponents,2*self.Nexponents))
-        
-        constraintMat[:self.NDataPoints,:self.Nexponents]=-(basis-np.absolute(basis))/2
-        constraintMat[self.NDataPoints:-self.Nexponents,:self.Nexponents]=(basis+np.absolute(basis))/2
-        constraintMat[:self.NDataPoints,self.Nexponents:]=-(basis+np.absolute(basis))/2
-        constraintMat[self.NDataPoints:-self.Nexponents,self.Nexponents:]=(basis-np.absolute(basis))/2
-        
-        constraintMat[-self.Nexponents:,:self.Nexponents]=np.eye(self.Nexponents)
-        constraintMat[-self.Nexponents:,self.Nexponents:]=-np.eye(self.Nexponents)
-            
-        b=np.zeros(((2*self.NDataPoints+self.Nexponents),1))
-        b[:self.NDataPoints,0]=-trainingOutput
-        b[self.NDataPoints:2*self.NDataPoints,0]=trainingOutput
-        
+        objective=np.concatenate((-basisSum,basisSum))
+
+        constraintMatrix=np.zeros((2*self.NDataPoints+self.Nterms,2*self.Nterms))
+
+        constraintMatrix[:self.NDataPoints,:self.Nterms]=-(basis-np.absolute(basis))/2
+        constraintMatrix[self.NDataPoints:-self.Nterms,:self.Nterms]=(basis+np.absolute(basis))/2
+        constraintMatrix[:self.NDataPoints,self.Nterms:]=-(basis+np.absolute(basis))/2
+        constraintMatrix[self.NDataPoints:-self.Nterms,self.Nterms:]=(basis-np.absolute(basis))/2
+
+        constraintMatrix[-self.Nterms:,:self.Nterms]=np.eye(self.Nterms)
+        constraintMatrix[-self.Nterms:,self.Nterms:]=-np.eye(self.Nterms)
+
+        b=np.zeros(((2*self.NDataPoints+self.Nterms),1))
+        b[:2*self.NDataPoints,0]=np.hstack((-trainingOutput,trainingOutput))
+
         from cvxopt import matrix, solvers
-              
-        sol=solvers.lp(matrix(objective),matrix(constraintMat),matrix(b))
-        
+
+        sol=solvers.lp(matrix(objective),matrix(constraintMatrix),matrix(b))
+
         self.paramVec=np.array(sol['x'])
-        
-        return self        
-    def predict(self,testInputs):
+
+        return self
+    def predict(self,testInput):
         try:
             getattr(self, "paramVec")
         except AttributeError:
             raise RuntimeError("You must train IPM before predicting data!")
-            
-        assert(testInputs.shape[1]==self.NFeatures),'The provided test data has the wrong number of features'
-        
-        NTestPoints=testInputs.shape[0]
-        
-        testInputs=testInputs/self.InputScale
-        
-        basis=np.zeros((self.NDataPoints,self.Nexponents))
-        
-        for j,exponentVector in enumerate(self.exponentList):
-            for i,inputSample in enumerate(testInputs):
-                basis[i,j]=np.prod(inputSample**exponentVector)      
-        
-        constraintMat=np.zeros((2*self.NDataPoints,2*self.Nexponents))
-        
-        constraintMat[:self.NDataPoints,:self.Nexponents]=-(basis-np.absolute(basis))/2
-        constraintMat[self.NDataPoints:,:self.Nexponents]=(basis+np.absolute(basis))/2
-        constraintMat[:self.NDataPoints,self.Nexponents:]=-(basis+np.absolute(basis))/2
-        constraintMat[self.NDataPoints:,self.Nexponents:]=(basis-np.absolute(basis))/2        
-        
-        out=np.dot(constraintMat,self.paramVec)
-        
-        return(-out[:NTestPoints],out[NTestPoints:])
-    def getModelReliability(self,confidence=1-10**-6):            
+
+        assert(testInput.shape[1]==self.NFeatures),'The provided test data has the wrong number of features'
+
+        NTestPoints=testInput.shape[0]
+
+        testInput=testInput/self.InputScale
+
+        poly = PolynomialFeatures(self.polynomialDegree)
+        basis=poly.fit_transform(testInput)
+
+        upperBound=0.5*np.dot(np.hstack((basis-np.absolute(basis),basis+np.absolute(basis))),self.paramVec)
+        lowerBound=0.5*np.dot(np.hstack((basis+np.absolute(basis),basis-np.absolute(basis))),self.paramVec)
+
+        return(upperBound,lowerBound)
+    def getModelReliability(self,confidence=1-10**-6):
         if confidence<0 or confidence>1:
             print('Invalid confidence parameter value')
         else:
-            return(1-2*self.Nexponents/((self.NDataPoints+1)*confidence))
-                
+            return(1-2*self.Nterms/((self.NDataPoints+1)*confidence))
