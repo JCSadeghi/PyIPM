080aa7c17a057a44c510244fccbf20250440da86
Jonathan Sadeghi <J.C.Sadeghi@liverpool.ac.uk>
2020-09-26 23:26:48 +0100

    Add type annotation

--- PyIPM.py
+++ PyIPM.py
@@ -5,6 +5,8 @@ import scipy.stats
 import scipy.misc
 import scipy.optimize
 
+from typing import Tuple
+
 # ==============================================================================
 #     This class is a port of the MATLAB code IntervalPredictorModel from
 #     OpenCossan
@@ -15,34 +17,38 @@ import scipy.optimize
 # ==============================================================================
 
 
-class PyIPM:
+class IPM:
     """
     A class to train and query Interval Predictor Models
     """
-    def __init__(self, polynomial_degree=1):
+    def __init__(self, polynomial_degree: int = 1):
         """
         Constructor for the Interval Predictor Model
-        :param polynomial_degree: Integer representing the degree of the fitted polynomial. Default 1.
+        Args:
+            polynomial_degree: Integer representing the degree of the fitted polynomial. Default 1.
         """
         self.polynomial_degree = polynomial_degree
-        assert (type(self.polynomial_degree) == int), 'polynomial_degree parameter must be integer'
+        if not isinstance(self.polynomial_degree, int):
+            raise ValueError("Polynomial Degree must be integer but is {}".format(self.polynomial_degree))
         self.n_features = None
         self.n_data_points = None
         self.input_scale = None
         self.n_terms = None
         self.param_vector = None
 
-    def fit(self, training_input, training_output):
+    def fit(self, training_input: np.ndarray, training_output: np.ndarray):
         """
         Fit the Interval Predictor Model to Training Data
-        :param training_input: NumPy array of IPM training inputs, dims: (n_samples x n_input_dimensions)
-        :param training_output: NumPy array of IPM training outputs, dims: (n_samples)
+        Args:
+            training_input: Array of IPM training inputs, dims: (n_samples x n_input_dimensions)
+            training_output: Array of IPM training outputs, dims: (n_samples)
+
         """
         self.n_features = training_input.shape[1]
         self.n_data_points = training_input.shape[0]
 
-        assert (training_output.shape == (
-            self.n_data_points,)), 'Number of input examples must equal number of output examples'
+        if not training_output.shape == (self.n_data_points,):
+            raise ValueError("Number of input examples must equal number of output examples")
 
         self.input_scale = np.mean(np.abs(training_input), axis=0)
         training_input = training_input / self.input_scale
@@ -71,16 +77,20 @@ class PyIPM:
 
         self.param_vector = np.array(sol['x'])
 
-    def predict(self, test_input):
+    def predict(self, test_input: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         """
         Make predictions from trained IPM
-        :param test_input: NumPy array of IPM test inputs, dims: (n_samples x n_input_dimensions)
-        :return: tuple containing upper and lower bound for test input
+        Args:
+            test_input: Array of IPM test inputs, dims: (n_samples x n_input_dimensions)
+
+        Returns:
+            Tuple containing upper and lower bound for test input
         """
         if self.param_vector is None:
             raise RuntimeError("You must train IPM before predicting data!")
 
-        assert (test_input.shape[1] == self.n_features), 'The provided test data has the wrong number of features'
+        if not test_input.shape[1] == self.n_features:
+            raise ValueError("The provided test data has the wrong number of features")
 
         test_input = test_input / self.input_scale
 
@@ -96,31 +106,41 @@ class PyIPM:
 
         return upper_bound, lower_bound
 
-    def get_model_reliability_old(self, confidence=1 - 10 ** -6):
+    def get_model_reliability_old(self, confidence: float = 1 - 10 ** -6) -> float:
         """
-        Compute the reliability of the trained IPM's prediction interval
-        :param confidence: the confidence with which the reliability is prescribed, float between 0 and 1
-        :return: reliability of the trained IPM's prediction interval, float between 0 and 1
+        Compute the reliability of the trained IPM's prediction interval using a conservative method
+        Args:
+            confidence: the confidence with which the reliability is prescribed, float between 0 and 1
+
+        Returns:
+            reliability of the trained IPM's prediction interval, float between 0 and 1
+
         """
         if confidence < 0 or confidence > 1:
-            print('Invalid confidence parameter value')
+            raise ValueError('Invalid confidence parameter value, must be between 0 and 1 but is {}'.format(confidence))
         else:
             return 1 - 2 * self.n_terms / ((self.n_data_points + 1) * (1-confidence))
 
-    def get_model_reliability(self, confidence=1 - 10 ** -6):
+    def get_model_reliability(self, confidence: float = 1 - 10 ** -6) -> float:
         """
         Compute the reliability of the trained IPM's prediction interval
-        :param confidence: the confidence with which the reliability is prescribed, float between 0 and 1
-        :return: reliability of the trained IPM's prediction interval, float between 0 and 1
+        Args:
+            confidence: the confidence with which the reliability is prescribed, float between 0 and 1
+
+        Returns:
+            reliability of the trained IPM's prediction interval, float between 0 and 1
+
         """
         if confidence < 0 or confidence > 1:
-            print('Invalid confidence parameter value')
+            raise ValueError('Invalid confidence parameter value, must be between 0 and 1 but is {}'.format(confidence))
         else:
             x0 = scipy.optimize.bisect(lambda epsilon: self.beta(epsilon) - (1 - confidence), 0, 1)
             reliability = 1 - x0
             return reliability
 
-    def beta(self, epsilon):
+    def beta(self, epsilon: float) -> float:
+        """
+        A helper function to compute model reliability
+        """
         d = 2 * self.n_terms
         return scipy.stats.binom.cdf(d - 1, self.n_data_points, epsilon)
-
